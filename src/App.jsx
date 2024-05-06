b2044cea951a01f212be97d075d05532c6afd0c5
krishna bisht <krishnabisht2003@gmail.com>
2024-05-06 19:08:38 +0530

    intital commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,6 +7,7 @@ function App() {
   return (
     <>
       <h1 className='bg-black uppercase text-white'>Hello world</h1>
+      <button onClick={()=>{setCount(count+1)}}>{count}</button>
     </>
   )
 }
