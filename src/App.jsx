ab7ffdd0c9a655a6e2afcffba24126f29bbf17d5
krishna bisht <krishnabisht2003@gmail.com>
2024-05-06 19:01:12 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-black uppercase text-white'>hello world</h1>
+      <h1 className='bg-black uppercase text-white'>Hello world</h1>
     </>
   )
 }
