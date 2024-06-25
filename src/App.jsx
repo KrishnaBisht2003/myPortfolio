c7277e60efc383800d6d199878cd55459f824ad2
krishna bisht <krishnabisht2003@gmail.com>
2024-06-25 23:42:38 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter } from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0)
+
+import {Hero, Navbar, About, Experience, Tech, Works, Feedbacks, Contact, StarsCanvas} from './components';
+
+
+const App = () => {
 
   return (
-    <>
-      <h1 className='bg-black uppercase text-white'>Hello world</h1>
-      <button onClick={()=>{setCount(count+1)}}>{count}</button>
-    </>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+          <Hero/>
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className="relative z-0">
+          <Contact/>
+          <StarsCanvas/>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
